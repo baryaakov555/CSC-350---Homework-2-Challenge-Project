776ce220ca690abec05dd48b17c7d636bf6f8d51
Bar Yaakov <baryaakov5050@gmail.com>
2025-09-07 13:29:50 -0400

    pushing the code from the guided project

--- Program.cs
+++ Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int examAssignments = 5;
+
+int[] sophiaScores = new int[] { 90, 86, 87, 98, 100, 94, 90 };
+int[] andrewScores = new int[] { 92, 89, 81, 96, 90, 89 };
+int[] emmaScores = new int[] { 90, 85, 87, 98, 68, 89, 89, 89 };
+int[] loganScores = new int[] { 90, 95, 87, 88, 96, 96 };
+int[] beckyScores = new int[] { 92, 91, 90, 91, 92, 92, 92 };
+int[] chrisScores = new int[] { 84, 86, 88, 90, 92, 94, 96, 98 };
+int[] ericScores = new int[] { 80, 90, 100, 80, 90, 100, 80, 90 };
+int[] gregorScores = new int[] { 91, 91, 91, 91, 91, 91, 91 };
+
+string[] studentNames = new string[] { "Sophia", "Andrew", "Emma", "Logan", "Becky", "Chris", "Eric", "Gregor" };
+int[] studentScores = new int[10];
+
+string currectStudentLetterGrade = "";
+
+Console.WriteLine("Student\t\tGrade\n");
+
+foreach (string name in studentNames)
+{
+    string currentStudent = name;
+
+    if (currentStudent == "Sophia")
+        studentScores = sophiaScores;
+    else if (currentStudent == "Andrew")
+        studentScores = andrewScores;
+    else if (currentStudent == "Emma")
+        studentScores = emmaScores;
+    else if (currentStudent == "Logan")
+        studentScores = loganScores;
+    else if (currentStudent == "Becky")
+        studentScores = beckyScores;
+    else if (currentStudent == "Chris")
+        studentScores = chrisScores;
+    else if (currentStudent == "Eric")
+        studentScores = ericScores;
+    else if (currentStudent == "Gregor")
+        studentScores = gregorScores;
+    else
+        continue;
+
+    int sumOfAssignmentScores = 0;
+    decimal currentStudentGrade = 0;
+
+    int assignmentNumber = 0;
+
+    foreach (int score in studentScores)
+    {
+        assignmentNumber += 1;
+
+        if (assignmentNumber <= examAssignments)
+            sumOfAssignmentScores += score;
+        else
+            sumOfAssignmentScores += score / 10;
+    }
+
+    currentStudentGrade = (decimal)sumOfAssignmentScores / examAssignments;
+
+    if (currentStudentGrade >= 97)
+        currectStudentLetterGrade = "A+";
+    else if (currentStudentGrade >= 93)
+        currectStudentLetterGrade = "A";
+    else if (currentStudentGrade >= 90)
+        currectStudentLetterGrade = "A-";
+    else if (currentStudentGrade >= 87)
+        currectStudentLetterGrade = "B+";
+    else if (currentStudentGrade >= 83)
+        currectStudentLetterGrade = "B";
+    else if (currentStudentGrade >= 80)
+        currectStudentLetterGrade = "B-";
+    else if (currentStudentGrade >= 77)
+        currectStudentLetterGrade = "C+";
+    else if (currentStudentGrade >= 73)
+        currectStudentLetterGrade = "C";
+    else if (currentStudentGrade >= 70)
+        currectStudentLetterGrade = "C-";
+    else if (currentStudentGrade >= 67)
+        currectStudentLetterGrade = "D+";
+    else if (currentStudentGrade >= 63)
+        currectStudentLetterGrade = "D";
+    else if (currentStudentGrade >= 60)
+        currectStudentLetterGrade = "D-";
+    else
+        currectStudentLetterGrade = "F";
+
+    Console.WriteLine($"{currentStudent}\t\t{currentStudentGrade}\t{currectStudentLetterGrade}");
+}
+
+Console.WriteLine("Press the Enter key to continue");
+Console.ReadLine();
